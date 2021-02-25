52f42a53aadfcec5e5d7f96f3484ea5a59788e21
FinocchiaroGR <A01704007@itesm.mx>
2021-02-25 17:27:34 -0600

    Actualizacion

---
+++ Video Juegos/RandomTest/RandomTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RandomTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new System.Random();
+            int randomnumber = rnd.Next(1, 10);
+            Console.WriteLine("randomnumber = {0}", randomnumber);
+
+            double randomdouble = rnd.NextDouble();
+            Console.WriteLine("randomdouble = {0}", randomdouble);
+        }
+    }
+}
---
+++ Video Juegos/SpaceShooter/Assets/Code/Enemy.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 5f;
+
+    public float bound_X = -11f;
+
+    private Animator anim;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 temp = transform.position;
+        temp.x -= speed * Time.deltaTime;
+
+        transform.position = temp;
+
+        if (temp.x < bound_X)
+            gameObject.SetActive(false);
+    }
+
+    void TurnOffObject()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.tag == "Bullet")
+        {
+            Invoke("TurnOffObject", 1f);
+
+            anim.Play("Explotion");
+        }
+
+    }
+}

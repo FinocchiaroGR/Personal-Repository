eda92445e6410999f0dc4da3e5f488d6fe47d14d
FinocchiaroGR <A01704007@itesm.mx>
2021-03-04 17:52:33 -0600

    Added Lab 7, lab 8, and part of the lab 9

---
+++ DAW & BD/Laboratorios/Laboratorio 1,3,5,7/JS/Node.js
@@ -0,0 +1,10 @@
+let arr = [5, 15, 35, 45, 55, 65, 75, 85 ,95, 100];
+function promedio(arr){
+    let sum = 0;
+    for(let i = 0; i < arr.lenght; i++){
+        sum += arr(i);
+    }
+    let promedio = sum / (arr.lenght + 1);
+    return promedio;
+}
+
